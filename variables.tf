92368fe7599f6544575b3119755211b6119b2d91
Boite Witte <info@boite.nl>
2020-03-09 15:20:39 +0100

    Update config to support terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,138 +1,142 @@
 variable "namespace" {
-  type = "string"
+  type        = string
   description = "The namespace for the Lambda function"
 }
 
 variable "environment" {
-  type = "string"
+  type        = string
   description = "The environment (stage) for the Lambda function"
 }
 
 variable "name" {
-  type = "string"
+  type        = string
   description = "A name for you Lambda Function"
 }
 
 variable "tags" {
-  type = "map"
+  type        = map(string)
   description = "describe your variable"
-  default = {}
+  default     = {}
 }
 
 variable "source_code_hash" {
-  type = "string"
+  type        = string
   description = "Used to trigger updates. Must be set to a base64-encoded SHA256 hash of the package file specified with either filename or s3_key."
 }
 
 variable "dead_letter_config" {
-  type = "map"
+  type        = map(string)
   description = "Nested block to configure the function's dead letter queue."
-  default = {}
+  default     = {}
 }
 
 variable "filename" {
-  type = "string"
+  type        = string
   description = "The path to the function's deployment package within the local filesystem."
 }
 
 variable "handler" {
-  type = "string"
+  type        = string
   description = "The function entrypoint in your code."
 }
 
 variable "environment_variables" {
-  type = "map"
+  type        = map(string)
   description = "A map that defines environment variables for the Lambda function."
-  default = {}
+  default     = {}
 }
 
 variable "memory_size" {
-  type = "string"
+  type        = string
   description = "Amount of memory in MB your Lambda Function can use at runtime. Defaults to 128."
-  default = "128"
+  default     = "128"
 }
 
 variable "runtime" {
-  type = "string"
+  type        = string
   description = "The runtime environment for the Lambda function you are uploading. (nodejs, nodejs4.3, nodejs6.10, java8, python2.7, python3.6, dotnetcore1.0, nodejs4.3-edge)"
 }
 
 variable "timeout" {
-  type = "string"
+  type        = string
   description = "The amount of time your Lambda Function has to run in seconds. Defaults to 3."
-  default = "3"
+  default     = "3"
 }
 
 variable "publish" {
-  type = "string"
+  type        = string
   description = "Whether to publish creation/change as new Lambda Function Version."
-  default = false
+  default     = false
 }
 
 variable "execution_policies_count" {
-  type = "string"
+  type        = string
   description = "Workaround for list of resources count"
-  default = "0"
+  default     = "0"
 }
 
 variable "execution_policies" {
-  type = "list"
+  type        = list(string)
   description = "The list of ARNs of the policies you want to apply to the Lambda execution role."
-  default = []
+  default     = []
 }
 
 variable "vpc_subnet_ids" {
-  type = "list"
+  type        = list(string)
   description = "A list of subnet IDs associated with the Lambda function."
-  default = []
+  default     = []
 }
 
 variable "vpc_security_group_ids" {
-  type = "list"
+  type        = list(string)
   description = "A list of security group IDs associated with the Lambda function."
-  default = []
+  default     = []
 }
 
 variable "logs_policy" {
+  type        = string
   description = "The name of the policy for the logs, if is set, then the logs policy will be created"
-  default = false
+  default     = false
 }
 
 variable "alias_name" {
-  type = "string"
+  type        = string
   description = "Name for the alias you are creating. Pattern: (?!^[0-9]+$)([a-zA-Z0-9-_]+), when false no alias is created"
-  default = ""
+  default     = ""
 }
 
 variable "alias_description" {
-  type = "string"
+  type        = string
   description = "Description of the alias."
-  default = ""
+  default     = ""
 }
 
 variable "alias_version" {
-  type = "string"
+  type        = string
   description = "Lambda function version for which you are creating the alias."
-  default = ""
+  default     = ""
 }
 
 variable "es_arn" {
-  type = "string"
+  type        = string
   description = "The event source ARN - can either be a Kinesis or DynamoDB stream. When false, no event stream is created"
-  default = ""
+  default     = ""
 }
 
 variable "es_enabled" {
+  type        = bool
   description = "Determines if the mapping will be enabled on creation. Defaults to true"
-  default = true
+  default     = true
 }
 
 variable "es_batch_size" {
+  type        = number
   description = "The largest number of records that Lambda will retrieve from your event source."
-  default = 100
+  default     = 100
 }
 
 variable "es_starting_position" {
+  type        = number
   description = "The position in the stream where AWS Lambda should start reading."
-  default = false
+  default     = null
 }
